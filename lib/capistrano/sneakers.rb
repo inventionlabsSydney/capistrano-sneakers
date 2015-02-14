b2a602599aafb0ea4e624d7acb04df39d6470fb1
Spirit <neverlandxy.naix@gmail.com>
2015-02-14 23:48:26 +0800

    add sneakers cap

--- lib/capistrano/sneakers.rb
+++ lib/capistrano/sneakers.rb
@@ -1,7 +1,7 @@
-require "capistrano/sneakers/version"
+require 'capistrano/sneakers/version'
 
-module Capistrano
-  module Sneakers
-    # Your code goes here...
-  end
+if Gem::Specification.find_by_name('capistrano').version >= Gem::Version.new('3.0.0')
+  load File.expand_path('../tasks/sidekiq.cap', __FILE__)
+else
+  raise Gem::LoadError, 'We are Sorry, this gem only for Capistrano3, please install Capistrano3 first.'
 end
