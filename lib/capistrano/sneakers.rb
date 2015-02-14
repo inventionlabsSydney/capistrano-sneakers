861647da529ee29ea4eff336f6536862a9c61710
Spirit <neverlandxy.naix@gmail.com>
2015-02-14 19:23:22 +0800

    init gem

---
+++ lib/capistrano/sneakers.rb
@@ -0,0 +1,7 @@
+require "capistrano/sneakers/version"
+
+module Capistrano
+  module Sneakers
+    # Your code goes here...
+  end
+end
---
+++ lib/capistrano/sneakers/version.rb
@@ -0,0 +1,5 @@
+module Capistrano
+  module Sneakers
+    VERSION = "0.0.1"
+  end
+end
