ad19b38b95ddd0b313269c992bc610711807825b
Karl Kloppenborg <karl@inventionlabs.com.au>
2015-03-11 12:04:35 +1100

    export workers

--- lib/capistrano/tasks/sneakers.rb
+++ lib/capistrano/tasks/sneakers.rb
@@ -93,8 +93,9 @@ namespace :sneakers do
       raise "[ set :workers, ['worker1', 'workerN'] ] not configured properly, please configure the workers you wish to use" if fetch(:sneakers_workers).nil? or fetch(:sneakers_workers) == false or !fetch(:sneakers_workers).kind_of? Array
 
       workers = fetch(:sneakers_workers).compact.join(',')
-      raise "Workers = #{workers}"
-      execute :bundle, :exec, :sneakers, args.compact.join(' ')
+      execute :export, workers #export this to environmental variable
+      info "Starting the sneakers processes"
+      #execute :bundle, :exec, :sneakers, args.compact.join(' ')
     else
       # Using custom sneakers setup
       args.push "--index #{idx}"
